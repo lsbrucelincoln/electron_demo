f34bcdbbcd44dc2c9dfec40ce323305cfaef7e9c
Lin Sen <276505128@qq.com>
2017-08-16 00:05:36 +0800

    完成基本的功能实现，主要在render.js里

--- renderer.js
+++ renderer.js
@@ -1,14 +1,33 @@
+var electron = require('electron');
+var axios = require('axios');
+var shell = electron.shell;
 checkUpdate()
-const axios = require('axios');
-const shell = electron.shell;
 
 function checkUpdate() {
+    axios.get('https://raw.githubusercontent.com/lsbrucelincoln/electron_demo/master/package.json?' + Date.parse(new Date()))
+        .then(function(response) {
+            var data = response.data;
+            // 获得本地版本进行比较
+            getLocalVersion(data)
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 
 }
-axios.get('https://raw.githubusercontent.com/iview/iview-cli/master/package.json?' + Date.parse(new Date()))
-    .then(function(response) {
-        console.log(response);
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+
+function getLocalVersion(data) {
+    axios.get('./package.json')
+        .then(function(response) {
+            var localData = response.data;
+            console.log('local version is ' + localData.version)
+            console.log('lastest version is ' + data.version)
+            if (data.version != localData.version) {
+                alert('请先下载最新的安装包')
+                shell.openExternal(localData.repository);
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
